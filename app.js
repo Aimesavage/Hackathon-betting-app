e30d6fd135d647b3e802951a4c58d324d52913aa
Aimé <aimewestone@gmail.com>
2024-02-10 19:34:03 -0500

    initiall commit

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express');
+const ejs = require('ejs')
+const app = express();
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+const path = require('path');
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
+
+
+app.get("/", (req, res)=>{
+    res.render("index")
+
+})
+
+app.get("/home", (req,res)=>{
+    res.render("home")
+})
+
+app.listen(3000, ()=>{
+    console.log("We are up and running")
+})
